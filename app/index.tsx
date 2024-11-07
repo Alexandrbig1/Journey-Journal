fa9c5679bab7a6bff0122bf7025b7b71c1928186
Alexandrbig1 <alexsmagin1@gmail.com>
2024-11-07 11:59:15 -0600

    feat: add registration component

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import RegistrationScreen from "../components/Screens/RegistrationScreen";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return <RegistrationScreen />;
+}
